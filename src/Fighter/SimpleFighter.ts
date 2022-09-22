b9cb95541848e7ed959121f6aa658a6044078c73
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-09-22 19:04:03 -0300

    feat: create simplefighter interface

---
+++ src/Fighter/SimpleFighter.ts
@@ -0,0 +1,10 @@
+interface SimpleFighter {
+  lifePoints: number;
+  strength: number;
+
+  attack(enemy: SimpleFighter): void;
+
+  receiveDamage(attackPoints: number): number;
+}
+
+export default SimpleFighter;
