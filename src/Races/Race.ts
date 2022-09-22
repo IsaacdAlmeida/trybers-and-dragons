f6884d81f7b6da6943fca08ec496227b156be246
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-09-22 14:28:21 -0300

    feat: create abstract class Race, now its possible extends other races

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+  
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+  
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  public static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  public abstract get maxLifePoints(): number;
+}
+
+export default Race;
